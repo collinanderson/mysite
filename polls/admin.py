3eb4f5a287203298f4dd3a2b0715600286bf2dff
Collin Anderson <collin@onetencommunications.com>
2014-08-01 19:24:15 -0400

    QuestionAdmin list_display
    By default, Django displays the str() of each object. But sometimes it’d
    be more helpful if we could display individual fields. To do that, use
    the list_display admin option, which is a tuple of field names to
    display, as columns, on the change list page for the object

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'],
