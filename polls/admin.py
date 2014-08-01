cc86be31a7508458c567bfcf01a92234b45e44b4
Collin Anderson <collin@onetencommunications.com>
2014-08-01 19:27:25 -0400

    search_fields = ['question_text']

--- polls/admin.py
+++ polls/admin.py
@@ -10,6 +10,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'],
