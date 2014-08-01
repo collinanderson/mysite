f24d3ae3721ea4d04db68a0586056ec953c80e13
Collin Anderson <collin@onetencommunications.com>
2014-08-01 19:01:51 -0400

    was_published_recently()

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +9,9 @@ class Question(models.Model):
 
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
