0fdea6755862945151a65380f0753eec37650673
Collin Anderson <collin@onetencommunications.com>
2014-08-01 20:03:06 -0400

    A shortcut: render()
    It’s a very common idiom to load a template, fill a context and return
    an HttpResponse object with the result of the rendered template. Django
    provides a shortcut. Here’s the full index() view, rewritten.

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,12 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from polls.models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
