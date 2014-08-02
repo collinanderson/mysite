01970f470f6fc071fb96d132b7a1a881da951d7e
Collin Anderson <collin@onetencommunications.com>
2014-08-01 20:15:10 -0400

    Raising a 404 error
    Now, let’s tackle the question detail view – the page that displays the
    question text for a given poll. Here’s the view.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from polls.models import Question
@@ -9,7 +10,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
