62a64302eca6495db70802bdfcc7cefe7b9ae2ad
Collin Anderson <collin@onetencommunications.com>
2014-08-01 20:01:46 -0400

    Now let’s update our index view in polls/views.py to use the template

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from polls.models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
