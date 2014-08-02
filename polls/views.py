8ebe855234007265775d68f713d3c7ac4eb13cc5
Collin Anderson <collin@onetencommunications.com>
2014-08-01 20:17:11 -0400

    A shortcut: get_object_or_404()
    It’s a very common idiom to use get() and raise Http404 if the object
    doesn’t exist. Django provides a shortcut. Here’s the detail() view,
    rewritten.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Question
 
@@ -10,10 +9,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
